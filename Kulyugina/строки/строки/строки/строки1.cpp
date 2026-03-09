8de8b64a96641d28808b1d0a215187ad9ca1534b
Anastasia <anastasia.kulyugina@gmail.com>
2026-03-09 21:23:47 +0300

    задание1

---
+++ Kulyugina/строки/строки/строки/строки1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s = "123oooeq777vkh efeiw53";
+    int count = 0;
+
+    for (char c : s) {
+        if (isdigit(c)) {
+            count++;
+        }
+    }
+
+    /*
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            count++;
+        }
+    }
+*/
+    cout << count << endl;
+    return 0;
+}
