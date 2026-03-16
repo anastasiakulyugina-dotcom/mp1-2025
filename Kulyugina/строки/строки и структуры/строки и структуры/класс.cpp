7b8f0f933a48f310bd3dce3d6c5db461a03b97c6
Anastasia <anastasia.kulyugina@gmail.com>
2026-03-16 21:53:22 +0300

    rkfcc Time

---
+++ Kulyugina/строки/строки и структуры/строки и структуры/класс.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Song {
+	string author;
+	string title;
+	int year;
+	double siz;
+
+public:
+	Song(string s1 = "", string s2 = "", int y = 2000, double d = 0.0);
+
+	void Print() {
+		cout << author << " " << title << " " << year << " " << siz << endl;
+	};
+
+	bool Compare(const Song& p2) {
+		if (siz < p2.siz) return true;
+		else return false;
+	};
+
+	Song::Song(string s1, string s2, int y, double d) : title(s1), author(s2), year(y), siz(d) {
+		cout << "construstor for " << s1 << "\n";
+	}
+};
+
+int main() {
+	Song p1("Beatles", "Yesterday", 1965, 2.06);
+	Song p2;
+
+	p1.Print();
+	bool a = p1.Compare(p2);
+	cout << a;
+
+
+
+	return 0;
+}
